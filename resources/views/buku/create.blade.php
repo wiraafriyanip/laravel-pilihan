fdcd9e8b9e607b00d0f8e37f9d13fee00acf79d9
wiraafriyanip <wiraafriyanip@gmail.com>
2023-05-29 15:37:41 +0700

    wira

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -10,8 +10,8 @@
                         <form action="{{ route('buku.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="form-group">
-                                <label class="font-weight-bold"> NAMA BUKU</label>
-                                <input type="text" class="form-control @error('judul_buku') is-invalid @enderror" name="judul_buku"value="{{ old('judul_buku') }}" placeholder="Masukkan Nama">
+                                <label class="font-weight-bold"> judul Buku</label>
+                                <input type="text" class="form-control @error('judul_buku') is-invalid @enderror" name="judul_buku"value="{{ old('judul_buku') }}" placeholder="Masukkan Nis">
 
                                 <!-- error message untuk title -->
                                 @error('judul_buku')
@@ -20,26 +20,29 @@
                                     </div>
                                     @enderror
                                 </div>
-                                <div class="form-group">
-                                    <label class="font-weight-bold">penerbit</label>
-                                    <input type="text" class="form-control@error('penerbit') is-invalid @enderror" name="penerbit" value="{{old('penerbit') }}" placeholder="Masukkan penerbit">
-                                    <!-- error message untuk title -->
-                                    @error('penerbit')
-                                        <div class="alert alert-danger mt-2">
-                                            {{ $message }}
-                                        </div>
+                            <div class="form-group">
+                                <label class="font-weight-bold"> penerbit</label>
+                                <input type="text" class="form-control @error('penerbit') is-invalid @enderror" name="penerbit"value="{{ old('penerbit') }}" placeholder="Masukkan penerbit">
+
+                                <!-- error message untuk title -->
+                                @error('penerbit')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label class="font-weight-bold">jumlah</label>
-                                    <input type="text" class="form-control@error('jumlah') is-invalid @enderror" name="jumlah" value="{{old('jumlah') }}" placeholder="Masukkan jumlah">
-                                    <!-- error message untuk title -->
-                                    @error('jumlah')
-                                        <div class="alert alert-danger mt-2">
-                                            {{ $message }}
-                                        </div>
+                                <label class="font-weight-bold"> Jumlah</label>
+                                <input type="text" class="form-control @error('jumlah') is-invalid @enderror" name="jumlah"value="{{ old('jumlah') }}" placeholder="Masukkan jumlah">
+
+                                <!-- error message untuk title -->
+                                @error('jumlah')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
+                               
                                 <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
                                 <button type="reset" class="btn btn-md btn-warning">RESET</button>
                                 </form>
